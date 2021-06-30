ed4f83db75ae624141daee192de019fabf21c17f
Phucngoc123 <86722347+Phucngoc123@users.noreply.github.com>
2021-06-30 22:31:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ const CONFIG = {
     Anh chỉ cười khi nơi đó có em 🥺`,
     btnIntro: 'khùng đin=))',
     title: 'Thật ra anh biết anh không có gì tốt hết🥺,chỉ được cái là rất yêu bé hoyy👉👈',
-    desc: 'Nên là bé đừng có ib nhiều với trai nho "ghen đó"😿 và chỉ yêu mình anh thôi nhé🥺',
+    desc: 'Nên là bé đừng có ib nhiều với trai nho "ghen đó"😿 với chỉ yêu mình anh thôi nhé🥺',
     btnYes: 'dạaa, yêuu anh nhất nhất <33',
     btnNo: 'Không thèm :))',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh dạ <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh dạ <33',
     btnReply: 'Ấn vào đây để gữi =))',
     reply: 'tại anh cute =))',
     mess: 'Anh biết mà 😳. Baby lúc nào thương anh nhất 😆🥰',
     messDesc: 'Mãi yêu nhau nhé công chúa của anh🥺.',
-    btnAccept: 'Okiiiii baby lun <3',
+    btnAccept: 'Okiiiii baby :))',
     messLink: 'https://www.facebook.com/beyencute2007'
 }
