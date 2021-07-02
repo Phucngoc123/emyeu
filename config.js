14f2b32e5f29eca650ce2f390849c567dc07e6fd
Phucngoc123 <86722347+Phucngoc123@users.noreply.github.com>
2021-07-02 15:16:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const CONFIG = {
     introDesc: `Bồ công anh bay khi có gió.
     Anh chỉ cười khi nơi đó có em 🥺`,
     btnIntro: 'khùng đin=))',
-    title: 'Thật ra anh biết anh không có gì tốt hết🥺,chỉ được cái là rất yêu bé hoyy👉👈',
-    desc: 'Nên là bé đừng có ib nhiều với trai nho "ghen đó"😿 với chỉ yêu mình anh thôi nhé🥺',
+    title: 'darling à! Hôm nay là một ngày thật đặc biệt nhể🥺, ngày mà 14 năm trước thế giới bé nhỏ của anh được sinh ra🥰',
+    desc: 'chúc công chúa của anh sinh nhật vui vẻ nhoa😘, ngày càng xinh đẹp đáng yêu học giỏi với yêu anh nhiều hơn nữa 👉👈',
     btnYes: 'dạaa, yêuu anh nhất nhất <33',
     btnNo: 'Không thèm :))',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh dạ <33',
+    question:'mà trên thế giới có hơn 7 tỉ người sao bé lại yêu anh dạ 🥺<3',
     btnReply: 'Ấn vào đây để gữi =))',
     reply: 'tại anh cute =))',
-    mess: 'Anh biết mà 😳. Baby lúc nào thương anh nhất 😆🥰',
-    messDesc: 'Mãi yêu nhau nhé công chúa của anh🥺.',
-    btnAccept: 'Okiiiii baby :))',
-    messLink: 'https://www.facebook.com/beyencute2007'
+    mess: 'Anh biết mà 😳. Baby lúc nào cũng thương anh nhất 😆🥰',
+    messDesc: 'mà sau này á 👉👈 có giận hờn gì thì nói ra rồi cùng nhau sửa nhen 🥺 nói nhỏ nhẹ là anh nghe òi :<< hicc, nói vậy thôi chứ yêu em nhấttt❤️ mãi bên nhau em nhé :33.',
+    btnAccept: 'dạ baby =))',
+    messLink: 'https://www.messenger.com/t/100022920264309'
 }
